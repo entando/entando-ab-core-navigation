e876b494d7173a9ca8808200b807bb80e8ee7286
Marco Corradetti <m.corradetti@entando.com>
2022-06-01 09:23:04 +0200

    update env settings

--- microfrontends/app-builder-menu/src/main.tsx
+++ microfrontends/app-builder-menu/src/main.tsx
@@ -1,9 +1,9 @@
-import { AppBuilderMenu } from "./custom-element";
+import { AppBuilderMenu } from './custom-element';
 
-if (process.env.NODE_ENV === "development") {
-  const { worker } = await import("../mocks/browser");
+if (import.meta.env.AB_MENU_USE_MOCKS === 'true') {
+  const { worker } = await import('../mocks/browser');
 
   await worker.start();
 }
 
-customElements.define("app-builder-menu", AppBuilderMenu);
+customElements.define('app-builder-menu', AppBuilderMenu);
--- microfrontends/app-builder-menu/src/vite-env.d.ts
+++ microfrontends/app-builder-menu/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly AB_MENU_USE_MOCKS: 'true' | 'false';
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}

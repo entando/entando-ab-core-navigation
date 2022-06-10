f8ef995126ae5d0aeecdc09d6de5583d8899173e
Irakli Chalagashvili <chalagashvili5@gmail.com>
2022-06-10 19:04:54 +0400

    ENG-3819 adjust to new shell globals

--- microfrontends/app-builder-menu/src/components/MenuUI.tsx
+++ microfrontends/app-builder-menu/src/components/MenuUI.tsx
@@ -78,10 +78,6 @@ export interface MfeConfig {
   api: {
     url: string;
   };
-  userPermissions: string[];
-  lang: string;
-  contentSchedulerPluginInstalled: boolean;
-  adminConsoleUrl: string;
 }
 
 interface Props {
@@ -91,8 +87,8 @@ interface Props {
 
 export function MenuUI(props: Props): JSX.Element {
   const { config, dynamicMenuItems } = props;
-  const { userPermissions, contentSchedulerPluginInstalled, adminConsoleUrl } =
-    config;
+  const { userPermissions, systemReport, adminConsoleUrl, lang } =
+    window.shellGlobals || {};
   const [activeListGroupItemId, setActiveListGroupItemId] = useState('');
   const [activeSecondaryMenuItemId, setActiveSecondaryMenuItemId] =
     useState('');
@@ -103,7 +99,8 @@ export function MenuUI(props: Props): JSX.Element {
 
   const navigate = useNavigation();
 
-  const activeLanguage = config.lang;
+  // @TODO Gui use this for language related code
+  const activeLanguage = lang;
 
   const pbcMenuItems = generateDynamicMenuItems(dynamicMenuItems);
 
@@ -302,16 +299,17 @@ export function MenuUI(props: Props): JSX.Element {
                   )}
                 />
               )}
-              {cmsHasMenuContentsAccess && contentSchedulerPluginInstalled && (
-                <SecondaryMenuItem
-                  id="content-scheduler"
-                  label={content.contentScheduler}
-                  href={convertToAdminConsoleUrl(
-                    adminConsoleUrl,
-                    'do/jpcontentscheduler/config/viewItem.action'
-                  )}
-                />
-              )}
+              {cmsHasMenuContentsAccess &&
+                systemReport?.contentSchedulerPluginInstalled && (
+                  <SecondaryMenuItem
+                    id="content-scheduler"
+                    label={content.contentScheduler}
+                    href={convertToAdminConsoleUrl(
+                      adminConsoleUrl,
+                      'do/jpcontentscheduler/config/viewItem.action'
+                    )}
+                  />
+                )}
               {cmsHasMenuContentTypeAccess && (
                 <SecondaryMenuItem
                   id="content-types"
--- microfrontends/app-builder-menu/src/types/api.ts
+++ microfrontends/app-builder-menu/src/types/api.ts
@@ -32,10 +32,20 @@ export interface DynamicMenuItem {
   children: ModifiedDynamicMenuItem[];
 }
 
+export interface ShellGlobals {
+  userPermissions: string[];
+  lang: string;
+  systemReport: {
+    contentSchedulerPluginInstalled: boolean;
+  };
+  adminConsoleUrl: string;
+}
+
 declare global {
   interface Window {
     appBuilderRouter: {
       push: (route: string) => void;
     };
+    shellGlobals: ShellGlobals;
   }
 }

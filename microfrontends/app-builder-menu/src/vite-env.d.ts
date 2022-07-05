d91d14916cb78dea51be637d7d9d9517d7c1e20a
Marco Corradetti <m.corradetti@entando.com>
2022-07-05 12:47:51 +0200

    integrate apiClaim in the dev environment

--- microfrontends/app-builder-menu/src/vite-env.d.ts
+++ microfrontends/app-builder-menu/src/vite-env.d.ts
@@ -1,21 +1,8 @@
 /// <reference types="vite/client" />
 
-import { rest } from "msw";
-
 interface ImportMetaEnv {
   readonly AB_MENU_USE_MOCKS: 'true' | 'false';
   readonly AB_MENU_EXPOSE_MOCKS: 'true' | 'false';
-}
-
-declare global {
-  interface Window {
-    entando: {
-      mockingService: {
-        setMock: (handler) => void,
-        restClient: typeof rest
-      }
-    }
-  }
 }
 
 interface ImportMeta {

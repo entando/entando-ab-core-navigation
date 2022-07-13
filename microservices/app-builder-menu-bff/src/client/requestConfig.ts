b2d62ae680c8db7b1c500d064e59c5e3861ccb14
zonia3000 <s.zorba@entando.com>
2022-07-13 09:02:19 +0200

    ENG-3941 Removed usage of 'any'

--- microservices/app-builder-menu-bff/src/client/requestConfig.ts
+++ microservices/app-builder-menu-bff/src/client/requestConfig.ts
@@ -1,15 +1,18 @@
 import { Request } from 'express';
-import { AxiosRequestConfig } from 'axios';
+import { AxiosRequestConfig, AxiosRequestHeaders } from 'axios';
 
-export function getRequestConfig(req: Request): AxiosRequestConfig<any> {
+type RequestConfig = AxiosRequestConfig<{
+    headers?: AxiosRequestHeaders
+}>
 
-    const requestConfig: AxiosRequestConfig<any> = {}
+export function getRequestConfig(req: Request) {
+    const requestConfig: RequestConfig = {};
 
     // Forward Authorization header
-    const authHeader = req.header('Authorization')
+    const authHeader = req.header('Authorization');
     if (authHeader !== undefined) {
-        requestConfig.headers = { Authorization: authHeader }
+        requestConfig.headers = { Authorization: authHeader };
     }
 
-    return requestConfig
+    return requestConfig;
 }

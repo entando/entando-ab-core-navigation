403f509b5eef23d72805e45e53ec332d1a5e79e5
Marco Corradetti <m.corradetti@entando.com>
2022-07-14 15:28:52 +0200

    add control operators for nullable fields

--- microfrontends/app-builder-menu/src/utils/getAPIEndpoint.ts
+++ microfrontends/app-builder-menu/src/utils/getAPIEndpoint.ts
@@ -3,7 +3,7 @@ import { MfeConfig } from '../types/globals';
 export type APIEndpointFields = "navigation"
 
 export function getAPIEndpoint(fieldName: APIEndpointFields, config: MfeConfig) {
-  const { url } = config.systemParams.api[fieldName];
+  const { url } = config?.systemParams?.api[fieldName] ?? {};
 
   return url.endsWith("/")
     ? url.substring(0, url.length - 1)

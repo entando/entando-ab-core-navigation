4fecc502691ed42b2071bf8e2a727c6da70fe26b
Irakli Chalagashvili <chalagashvili5@gmail.com>
2022-09-06 15:12:39 +0400

    ENG-4154 amend and improve tests

---
+++ microfrontends/app-builder-menu/src/components/SecondaryMenu/SecondaryMenu.test.tsx
@@ -0,0 +1,41 @@
+import {
+  render,
+  waitForElementToBeRemoved,
+  screen,
+  fireEvent
+} from '@testing-library/react';
+import { Menu } from '../Menu';
+
+test('Check toggling secondary menu', async () => {
+  window.entando.globals = {
+    ...window.entando.globals,
+    userPermissions: ['managePages']
+  };
+  window.entando.router = { push: jest.fn((val: string) => {}) };
+  render(
+    <Menu
+      config={{
+        systemParams: {
+          api: {
+            navigation: {
+              url: 'http://localhost:8080'
+            }
+          }
+        }
+      }}
+    />
+  );
+
+  await waitForElementToBeRemoved(screen.queryByText('Loading...'));
+
+  expect(screen.getByText('Pages')).toBeInTheDocument();
+  // Check that secondary menu is not visible
+  expect(screen.queryByText('Management')).not.toBeInTheDocument();
+  fireEvent.click(screen.getByText('Pages'));
+  // Check that secondary menu is visible
+  expect(screen.getByText('Management')).toBeInTheDocument();
+  fireEvent.click(screen.getByText('Dashboard'));
+  // Check that secondary menu is not visible
+  expect(screen.queryByText('Management')).not.toBeInTheDocument();
+  expect(window.entando.router.push).toHaveBeenCalledWith('/dashboard');
+});
--- microfrontends/app-builder-menu/src/components/hooks/useContents.test.tsx
+++ microfrontends/app-builder-menu/src/components/hooks/useContents.test.tsx
@@ -1,9 +1,39 @@
-import { useContent } from './useContent';
-import { renderHook } from '@testing-library/react';
+import { ContentProvider, useContent } from './useContent';
+import { render, screen } from '@testing-library/react';
+import { IntlProvider } from 'react-intl';
+import en_messages from '../../i18n/en.json';
+
+const MOCKED_CONTENT = {
+  'app-builder-menu': {
+    id: '1',
+    title: 'App Builder Menu'
+  }
+};
+
+jest.mock('../../content.ts', () => ({
+  getContent: jest.fn(() => MOCKED_CONTENT)
+}));
+
+const TestComponent = () => {
+  const content = useContent();
+  return <div>{JSON.stringify(content)}</div>;
+};
+
+const TestComponentWrapper = () => {
+  return (
+    <IntlProvider locale="en" defaultLocale="en" messages={en_messages}>
+      <ContentProvider>
+        <TestComponent />
+      </ContentProvider>
+    </IntlProvider>
+  );
+};
 
 describe('useContent', () => {
-  it('must render a correct object', async () => {
-    const { result } = renderHook(() => useContent());
-    expect(result.current).toBeInstanceOf(Object);
+  it('must render a correct content object', async () => {
+    render(<TestComponentWrapper />);
+    expect(
+      screen.getByText(JSON.stringify(MOCKED_CONTENT))
+    ).toBeInTheDocument();
   });
 });
